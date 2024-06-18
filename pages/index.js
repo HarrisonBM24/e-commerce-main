56ef423b99ef9eee61dfdfdb2e4097dd5f00a0b9
HarrisonBM24 <mharry2404@gmail.com>
2024-06-17 22:00:07 -0400

    - Feat: Added newProducts - Refactor: Renamed variables to better refer to their function and code order

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,30 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import NewProducts from "@/components/NewProduct";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 
-export default function HomePage({ product }) {
+export default function HomePage({ featuredProduct, newProducts }) {
   return (
     <div>
       <Header />
-      <Featured product={product} />
+      <Featured product={featuredProduct} />
+      <NewProducts products={newProducts} />
     </div>
   );
 }
 
 export async function getServerSideProps() {
-  const featuredProductId = "667007fcfb39ceb2aa082740";
   await mongooseConnect();
-  const product = await Product.findById(featuredProductId);
+
+  const featuredProductId = "667007fcfb39ceb2aa082740";
+  const featuredProduct = await Product.findById(featuredProductId);
+  const newProducts = await Product.find({}, null, { sort: { _id: -1 } });
 
   return {
-    props: { product: JSON.parse(JSON.stringify(product)) },
+    props: {
+      featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
+      newProducts: JSON.parse(JSON.stringify(newProducts)),
+    },
   };
 }
