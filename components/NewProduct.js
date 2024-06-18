04870ba0d28ccf31a720680d694a88d8c0dc1636
HarrisonBM24 <mharry2404@gmail.com>
2024-06-17 22:33:49 -0400

    Added: Component NewProducts to handle products and list them

---
+++ components/NewProduct.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import Center from "./Center";
+import ProductBox from "./ProductBox";
+
+const ProductsGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 60px;
+  padding-top: 40px;
+`;
+
+export default function NewProducts({ products }) {
+  return (
+    <Center>
+      <ProductsGrid>
+        {products?.length > 0 &&
+          products.map((product) => (
+            <ProductBox key={product._id} {...product} />
+          ))}
+      </ProductsGrid>
+    </Center>
+  );
+}
