c1e56ee96688963913f2e864c1b155c654313fce
HarrisonBM24 <mharry2404@gmail.com>
2024-06-18 22:15:30 -0400

    Feat: Added new styles

--- components/NewProduct.js
+++ components/NewProduct.js
@@ -6,12 +6,18 @@ const ProductsGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 60px;
-  padding-top: 40px;
+`;
+
+const Title = styled.h2`
+  font-size: 2rem;
+  font-weight: 700;
+  margin: 30px 0 20px;
 `;
 
 export default function NewProducts({ products }) {
   return (
     <Center>
+      <Title>New Arrivals</Title>
       <ProductsGrid>
         {products?.length > 0 &&
           products.map((product) => (
